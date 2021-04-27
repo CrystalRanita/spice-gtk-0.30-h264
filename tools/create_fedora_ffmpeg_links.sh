5a7a02880ba7c5af1f943ec631dfe73ee51463d6
crystal.lin <crystal.lin@acer.com>
2021-04-27 16:47:56 +0800

    Create Fedora Links

---
+++ tools/create_fedora_ffmpeg_links.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo ln -s /usr/include/ffmpeg/libswscale /usr/include/libswscale
+sudo ln -s /usr/include/ffmpeg/libavcodec /usr/include/libavcodec
+sudo ln -s /usr/include/ffmpeg/libavutil  /usr/include/libavutil
